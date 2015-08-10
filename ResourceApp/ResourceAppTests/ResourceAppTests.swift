1859f3a80cefacd449c9608a2d6625a56a5b0ba3
Mathijs Kadijk <mkadijk@gmail.com>
2015-08-10 17:39:03 +0200

    Create testcase for duplicate xib

--- ResourceApp/ResourceAppTests/ResourceAppTests.swift
+++ ResourceApp/ResourceAppTests/ResourceAppTests.swift
@@ -13,7 +13,8 @@ import XCTest
 class ResourceAppTests: XCTestCase {
 
   let expectedWarnings = [
-    "warning: [R.swift] Skipping 2 images because symbol 'second' would be generated for all of these images: Second, second"
+    "warning: [R.swift] Skipping 2 images because symbol 'second' would be generated for all of these images: Second, second",
+    "warning: [R.swift] Skipping 2 xibs because symbol 'duplicate' would be generated for all of these xibs: duplicate, Duplicate",
   ]
   
   func testLoggedWarnings() {
