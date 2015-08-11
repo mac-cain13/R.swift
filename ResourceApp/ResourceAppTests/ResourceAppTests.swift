11f915bbfbb58d6d8fab588104cafa0cb499b9bb
Mathijs Kadijk <mkadijk@gmail.com>
2015-08-11 21:01:59 +0200

    Create testcase for duplicate storyboards

--- ResourceApp/ResourceAppTests/ResourceAppTests.swift
+++ ResourceApp/ResourceAppTests/ResourceAppTests.swift
@@ -15,6 +15,7 @@ class ResourceAppTests: XCTestCase {
   let expectedWarnings = [
     "warning: [R.swift] Skipping 2 images because symbol 'second' would be generated for all of these images: Second, second",
     "warning: [R.swift] Skipping 2 xibs because symbol 'duplicate' would be generated for all of these xibs: duplicate, Duplicate",
+    "warning: [R.swift] Skipping 2 storyboards because symbol 'duplicate' would be generated for all of these storyboards: duplicate, Duplicate",
   ]
   
   func testLoggedWarnings() {
