2e521234422e767be78863dd9993d1d863447943
Mathijs Kadijk <mkadijk@gmail.com>
2015-09-28 17:46:34 +0200

    Create empty test to make the test runner happy

---
+++ R.swiftTests/MainTests.swift
@@ -0,0 +1,35 @@
+//
+//  MainTests.swift
+//  R.swift
+//
+//  Created by Mathijs Kadijk on 28-09-15.
+//  Copyright © 2015 Mathijs Kadijk. All rights reserved.
+//
+
+import XCTest
+
+class MainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
