29570b04d31d87b7e131d6ef7660f87d1d98391d
Mathijs Kadijk <mkadijk@gmail.com>
2015-12-22 08:24:03 +0100

    Fix validation related tests

--- ResourceApp/ResourceApp/AppDelegate.swift
+++ ResourceApp/ResourceApp/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
-    R.assertValid()
-
     return true
   }
 
