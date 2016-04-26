6a4f9ae42a10066b968936dc15743144b900090a
Mathijs Kadijk <mkadijk@gmail.com>
2016-04-26 22:08:01 +0200

    More clear statement of code reuse from SwiftGen

--- R.swift/ResourceTypes/FormatSpecifier.swift
+++ R.swift/ResourceTypes/FormatSpecifier.swift
@@ -5,9 +5,15 @@
 //  Created by Tom Lokhorst on 2016-04-18.
 //  Copyright © 2016 Mathijs Kadijk. All rights reserved.
 //
+//  Parts of the content of this file are loosly based on StringsFileParser.swift from SwiftGen/GenumKit.
+//  We don't feel this is a "substantial portion of the Software" so are not including their MIT license,
+//  eventhough we would like to give credit where credit is due by referring to SwiftGen thanking Olivier 
+//  Halligon for creating SwiftGen and GenumKit.
+//
+//  See: https://github.com/AliSoftware/SwiftGen/blob/master/GenumKit/Parsers/StringsFileParser.swift
+//
 
 import Foundation
-
 
 // https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/Strings/Articles/formatSpecifiers.html#//apple_ref/doc/uid/TP40004265-SW1
 enum FormatSpecifier {
@@ -76,8 +82,6 @@ extension FormatSpecifier {
     return _formatSpecifiersFromFormatString(formatString)
   }
 }
-
-// Based on StringsFileParser.swift from SwiftGen
 
 private let formatTypesRegEx: NSRegularExpression = {
   let pattern_int = "(?:h|hh|l|ll|q|z|t|j)?([dioux])" // %d/%i/%o/%u/%x with their optional length modifiers like in "%lld"
