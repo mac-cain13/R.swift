3b9a4a7da4011e847242df1524dfa9f25b81a6bc
Kasper T <ktv@trifork.com>
2016-04-26 19:50:20 +0200

    Adding error / warning messages to parsing stage when errors occur.

--- R.swift/ResourceTypes/Nib.swift
+++ R.swift/ResourceTypes/Nib.swift
@@ -9,99 +9,123 @@
 import Foundation
 
 private let ElementNameToTypeMapping = [
-  // TODO: Should contain all standard view elements, like button -> UIButton, view -> UIView etc
-  "view": Type._UIView,
-  "tableViewCell": Type._UITableViewCell,
-  "collectionViewCell": Type._UICollectionViewCell,
-  "collectionReusableView": Type._UICollectionReusableView
+    // TODO: Should contain all standard view elements, like button -> UIButton, view -> UIView etc
+    "view": Type._UIView,
+    "tableViewCell": Type._UITableViewCell,
+    "collectionViewCell": Type._UICollectionViewCell,
+    "collectionReusableView": Type._UICollectionReusableView
 ]
 
 struct Nib: WhiteListedExtensionsResourceType, ReusableContainer {
-  static let supportedExtensions: Set<String> = ["xib"]
-
-  let name: String
-  let rootViews: [Type]
-  let reusables: [Reusable]
-
-  init(url: NSURL) throws {
-    try Nib.throwIfUnsupportedExtension(url.pathExtension)
-
-    name = url.filename!
-
-    let parserDelegate = NibParserDelegate();
-
-    let parser = NSXMLParser(contentsOfURL: url)!
-    parser.delegate = parserDelegate
-    parser.parse()
-
-    rootViews = parserDelegate.rootViews
-    reusables = parserDelegate.reusables
-  }
+    static let supportedExtensions: Set<String> = ["xib"]
+    
+    let name: String
+    let rootViews: [Type]
+    let reusables: [Reusable]
+    
+    init(url: NSURL) throws {
+        try Nib.throwIfUnsupportedExtension(url.pathExtension)
+        
+        name = url.filename!
+        let fullPath = url.path!
+        
+        let parserDelegate = NibParserDelegate();
+        parserDelegate.filename = name
+        
+        //load data before, so we can detect if there is a problem loading it
+        let data = NSData(contentsOfURL: url.filePathURL!)
+        guard let workingData = data else {
+            //if we could not load the data, warn and return
+            warn("Failed to read file : \(fullPath)")
+            rootViews = [Type]()
+            reusables = [Reusable]()
+            return
+        }
+        //try parse the document
+        let parser =  NSXMLParser(data: workingData)
+        parser.delegate = parserDelegate
+        if !parser.parse() {
+            //if we fail at parsing, then warn, this is for example bad xml, or if no data was presented at all
+            warn("Could not parse file : \(fullPath)")
+        }
+        rootViews = parserDelegate.rootViews
+        reusables = parserDelegate.reusables
+    }
 }
 
 private class NibParserDelegate: NSObject, NSXMLParserDelegate {
-  let ignoredRootViewElements = ["placeholder"]
-  var rootViews: [Type] = []
-  var reusables: [Reusable] = []
-
-  // State
-  var isObjectsTagOpened = false;
-  var levelSinceObjectsTagOpened = 0;
-
-  @objc func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-    switch elementName {
-    case "objects":
-      isObjectsTagOpened = true;
-
-    default:
-      if isObjectsTagOpened {
-        levelSinceObjectsTagOpened += 1;
-
-        if let rootView = viewWithAttributes(attributeDict, elementName: elementName)
-          where levelSinceObjectsTagOpened == 1 && ignoredRootViewElements.filter({ $0 == elementName }).count == 0 {
-            rootViews.append(rootView)
+    let ignoredRootViewElements = ["placeholder"]
+    var rootViews: [Type] = []
+    var reusables: [Reusable] = []
+    
+    // State
+    var isObjectsTagOpened = false;
+    var filename : String?
+    var levelSinceObjectsTagOpened = 0;
+    
+    @objc func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        switch elementName {
+        case "objects":
+            isObjectsTagOpened = true;
+            
+        default:
+            if isObjectsTagOpened {
+                levelSinceObjectsTagOpened += 1;
+                
+                if let rootView = viewWithAttributes(attributeDict, elementName: elementName)
+                    where levelSinceObjectsTagOpened == 1 && ignoredRootViewElements.filter({ $0 == elementName }).count == 0 {
+                    rootViews.append(rootView)
+                }
+            }
+            
+            if let reusable = reusableFromAttributes(attributeDict, elementName: elementName) {
+                reusables.append(reusable)
+            }
         }
-      }
-
-      if let reusable = reusableFromAttributes(attributeDict, elementName: elementName) {
-        reusables.append(reusable)
-      }
     }
-  }
-
-  @objc func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-    switch elementName {
-    case "objects":
-      isObjectsTagOpened = false;
-
-    default:
-      if isObjectsTagOpened {
-        levelSinceObjectsTagOpened -= 1;
-      }
+    
+    @objc func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch elementName {
+        case "objects":
+            isObjectsTagOpened = false;
+            
+        default:
+            if isObjectsTagOpened {
+                levelSinceObjectsTagOpened -= 1;
+            }
+        }
     }
-  }
-
-  func viewWithAttributes(attributeDict: [String : String], elementName: String) -> Type? {
-    let customModuleProvider = attributeDict["customModuleProvider"]
-    let customModule = (customModuleProvider == "target") ? nil : attributeDict["customModule"]
-    let customClass = attributeDict["customClass"]
-    let customType = customClass.map { Type(module: Module(name: customModule), name: $0, optional: false) }
-
-    return customType ?? ElementNameToTypeMapping[elementName] ?? Type._UIView
-  }
-
-  func reusableFromAttributes(attributeDict: [String : String], elementName: String) -> Reusable? {
-    guard let reuseIdentifier = attributeDict["reuseIdentifier"] where reuseIdentifier != "" else {
-      return nil
+    
+    @objc func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        var message = parseError.localizedDescription
+        //try extract the xml parser error message.
+        if let errorMessage = parseError.userInfo["NSXMLParserErrorMessage"] as? String {
+            message = errorMessage
+        }
+        warn("Error parsing \(filename ?? "") , code: \(parseError.code), Description: \(message), Line: \(parser.lineNumber), Column: \(parser.columnNumber) ")
     }
-
-    let customModuleProvider = attributeDict["customModuleProvider"]
-    let customModule = (customModuleProvider == "target") ? nil : attributeDict["customModule"]
-    let customClass = attributeDict["customClass"]
-    let customType = customClass.map { Type(module: Module(name: customModule), name: $0, optional: false) }
-
-    let type = customType ?? ElementNameToTypeMapping[elementName] ?? Type._UIView
-
-    return Reusable(identifier: reuseIdentifier, type: type)
-  }
+    
+    func viewWithAttributes(attributeDict: [String : String], elementName: String) -> Type? {
+        let customModuleProvider = attributeDict["customModuleProvider"]
+        let customModule = (customModuleProvider == "target") ? nil : attributeDict["customModule"]
+        let customClass = attributeDict["customClass"]
+        let customType = customClass.map { Type(module: Module(name: customModule), name: $0, optional: false) }
+        
+        return customType ?? ElementNameToTypeMapping[elementName] ?? Type._UIView
+    }
+    
+    func reusableFromAttributes(attributeDict: [String : String], elementName: String) -> Reusable? {
+        guard let reuseIdentifier = attributeDict["reuseIdentifier"] where reuseIdentifier != "" else {
+            return nil
+        }
+        
+        let customModuleProvider = attributeDict["customModuleProvider"]
+        let customModule = (customModuleProvider == "target") ? nil : attributeDict["customModule"]
+        let customClass = attributeDict["customClass"]
+        let customType = customClass.map { Type(module: Module(name: customModule), name: $0, optional: false) }
+        
+        let type = customType ?? ElementNameToTypeMapping[elementName] ?? Type._UIView
+        
+        return Reusable(identifier: reuseIdentifier, type: type)
+    }
 }
