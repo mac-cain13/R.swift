7c9d4005cc22d0c4eb057ce3c41b14da9ce51247
Mathijs Kadijk <mkadijk@gmail.com>
2016-10-03 18:17:10 +0200

    Remove Apple template comments from testcase

--- R.swiftTests/MainTests.swift
+++ R.swiftTests/MainTests.swift
@@ -12,11 +12,9 @@ class MainTests: XCTestCase {
     
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
   
   override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
 
@@ -37,9 +35,6 @@ class MainTests: XCTestCase {
   ]
   
   func testSwiftNameSanitization() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-
     swiftNameData.forEach {
       let sanitizedResult = SwiftIdentifier(name: $0.0, lowercaseFirstCharacter: true).description
       XCTAssertEqual(sanitizedResult, $0.1)
