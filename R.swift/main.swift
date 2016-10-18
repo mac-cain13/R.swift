3e4f7a41c5f91e6eac9ee150917d474a46ede6eb
shiraji <isogai.shiraji@gmail.com>
2016-10-19 00:13:51 +0900

    Filter skipped resources

--- R.swift/main.swift
+++ R.swift/main.swift
@@ -13,15 +13,29 @@ let IndentationString = "  "
 let ResourceFilename = "R.generated.swift"
 
 var isEdgeEnabled = false
+private func isValidURL(ignoreFile : IgnoreFile?, url: NSURL) -> Bool {
+  if let ignoreFile = ignoreFile {
+    return !ignoreFile.match(url: url)
+  } else {
+    return true
+  }
+}
 
 do {
   let callInformation = try CallInformation(processInfo: ProcessInfo())
   isEdgeEnabled = callInformation.edge
 
   let xcodeproj = try Xcodeproj(url: callInformation.xcodeprojURL)
+
+  var ignoreFile : IgnoreFile? = nil
+  if let rswiftignoreURL = callInformation.rswiftignoreURL {
+    ignoreFile = try IgnoreFile(ignoreFileURL: rswiftignoreURL)
+  }
+
   let resourceURLs = try xcodeproj.resourcePathsForTarget(callInformation.targetName)
     .map(pathResolver(with: callInformation.URLForSourceTreeFolder))
     .flatMap { $0 }
+    .filter { isValidURL(ignoreFile: ignoreFile, url: $0 as NSURL) }
 
   let resources = Resources(resourceURLs: resourceURLs, fileManager: FileManager.default)
 
