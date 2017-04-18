13a0bd3aae61f66d2a04d6753c7fbe491488bae4
Nolan Warner <renrawnalon@gmail.com>
2017-04-18 15:45:26 +0900

    Add DisplayP3 tests

--- ResourceApp/ResourceAppTests/ColorsTests.swift
+++ ResourceApp/ResourceAppTests/ColorsTests.swift
@@ -16,6 +16,27 @@ class ColorsTests: XCTestCase {
     XCTAssertNotNil(R.color.myRSwiftColors.allIsAOK())
     XCTAssertNotNil(R.color.myRSwiftColors.severeError())
     XCTAssertNotNil(R.color.myRSwiftColors.seeThroughGray)
+    XCTAssertNotNil(R.color.displayP3.red())
+    XCTAssertNotNil(R.color.displayP3.green())
+  }
+  
+  func testDisplayP3Colors() {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    
+    R.color.displayP3.red().getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    XCTAssertTrue(round(red * 255) == 255)
+    XCTAssertTrue(round(green * 255) == 0)
+    XCTAssertTrue(round(blue * 255) == 0)
+    XCTAssertTrue(round(alpha * 255) == 255)
+    
+    R.color.displayP3.green().getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    XCTAssertTrue(round(red * 255) == 0)
+    XCTAssertTrue(round(green * 255) == 255)
+    XCTAssertTrue(round(blue * 255) == 0)
+    XCTAssertTrue(round(alpha * 255) == 255)
   }
 
 }
