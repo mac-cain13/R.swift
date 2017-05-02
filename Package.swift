3c46a2553f8266c8b3199f13f80e25276ec62ca4
Tom Lokhorst <tom@lokhorst.eu>
2017-05-02 17:52:32 +0200

    Remove not needed test target

--- Package.swift
+++ Package.swift
@@ -8,10 +8,6 @@ let package = Package(
       dependencies: ["RswiftCore"]
     ),
     Target(name: "RswiftCore"),
-    Target(
-      name: "RswiftCoreTests",
-      dependencies: ["RswiftCore"]
-    ),
   ],
   dependencies: [
     .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
