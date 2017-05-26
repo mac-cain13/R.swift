5591370c63da81d432943cfd4e668e03207ca214
Guido Sabatini <guido.sabatini@sysdata.it>
2017-05-26 11:38:34 +0200

    regex fix for trailing zeros

--- Sources/RswiftCore/ResourceTypes/StringParam.swift
+++ Sources/RswiftCore/ResourceTypes/StringParam.swift
@@ -170,7 +170,7 @@ private let formatTypesRegEx: NSRegularExpression = {
   let pattern_int = "(?:h|hh|l|ll|q|z|t|j)?([dioux])" // %d/%i/%o/%u/%x with their optional length modifiers like in "%lld"
   let pattern_float = "[aefg]"
   let position = "([1-9]\\d*\\$)?" // like in "%3$" to make positional specifiers
-  let precision = "[-+]?\\d?(?:\\.\\d)?" // precision like in "%1.2f"
+  let precision = "[-+]?\\d*(?:\\.\\d*)?" // precision like in "%1.2f"
   let reference = "#@([^@]+)@" // reference to NSStringFormatSpecType in .stringsdict
   do {
     return try NSRegularExpression(pattern: "(?<!%)%\(position)\(precision)(@|\(pattern_int)|\(pattern_float)|[csp]|\(reference))", options: [.caseInsensitive])
