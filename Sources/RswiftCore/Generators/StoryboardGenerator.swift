78069c33a458f24e6f6fcdb98c1ec81e18cc0644
cooler333 <utm4@mail.ru>
2018-11-12 12:26:57 +0300

    add fix for colors

--- Sources/RswiftCore/Generators/StoryboardGenerator.swift
+++ Sources/RswiftCore/Generators/StoryboardGenerator.swift
@@ -164,7 +164,7 @@ struct StoryboardStructGenerator: StructGenerator {
       }
     let validateColorLines = storyboard.usedColorResources.uniqueAndSorted()
       .map {
-        "if UIKit.UIColor(named: \"\($0)\") == nil { throw Rswift.ValidationError(description: \"[R.swift] Color named '\($0)' is used in storyboard '\(storyboard.name)', but couldn't be loaded.\") }"
+        "if UIKit.UIColor(named: \"\($0)\", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: \"[R.swift] Color named '\($0)' is used in storyboard '\(storyboard.name)', but couldn't be loaded.\") }"
       }
     let validateColorLinesWithAvailableIf = ["if #available(iOS 11.0, *) {"] +
       validateColorLines.map { $0.indent(with: "  ") } +
