f9a8e8f8f73ed1103434253fa27a56118d95c5d7
Brian Clymer <crdnilfan@gmail.com>
2019-05-08 15:43:45 -0500

    Slight image processing performance tweak.

--- Sources/RswiftCore/ResourceTypes/Image.swift
+++ Sources/RswiftCore/ResourceTypes/Image.swift
@@ -12,6 +12,8 @@ import Foundation
 struct Image: WhiteListedExtensionsResourceType {
   // See "Supported Image Formats" on https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIImage_Class/
   static let supportedExtensions: Set<String> = ["tiff", "tif", "jpg", "jpeg", "gif", "png", "bmp", "bmpf", "ico", "cur", "xbm"]
+  private static let extensions = Image.supportedExtensions.joined(separator: "|")
+  private static let regex = try! NSRegularExpression(pattern: "(~(ipad|iphone))?(@[2,3]x)?\\.(\(extensions))$", options: .caseInsensitive)
 
   let name: String
 
@@ -24,10 +26,8 @@ struct Image: WhiteListedExtensionsResourceType {
       throw ResourceParsingError.parsingFailed("Filename and/or extension could not be parsed from URL: \(url.absoluteString)")
     }
 
-    let extensions = Image.supportedExtensions.joined(separator: "|")
-    let regex = try! NSRegularExpression(pattern: "(~(ipad|iphone))?(@[2,3]x)?\\.(\(extensions))$", options: .caseInsensitive)
     let fullFileNameRange = NSRange(location: 0, length: filename.count)
     let pathExtensionToUse = (pathExtension == "png") ? "" : ".\(pathExtension)"
-    name = regex.stringByReplacingMatches(in: filename, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: fullFileNameRange, withTemplate: pathExtensionToUse)
+    name = Image.regex.stringByReplacingMatches(in: filename, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: fullFileNameRange, withTemplate: pathExtensionToUse)
   }
 }
