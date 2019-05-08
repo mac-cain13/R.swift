37ab03f8ae5e6e3cb0d6d9bcf8d17aba926bcb48
Brian Clymer <crdnilfan@gmail.com>
2019-05-08 15:44:25 -0500

    Append all images that are missing from xibs and storyboards to end of file.

--- Sources/RswiftCore/RswiftCore.swift
+++ Sources/RswiftCore/RswiftCore.swift
@@ -69,6 +69,21 @@ public struct RswiftCore {
       if callInformation.objcCompat {
         fileContents += objcConvertibles.compactMap { $0.objcCode(prefix: "") }.joined(separator: "\n") + "\n"
       }
+        
+      let allImages =
+        resources.images.map { $0.name } +
+        resources.assetFolders.flatMap { $0.imageAssets }
+
+      let allUsedImages =
+        resources.nibs.flatMap { $0.usedImageIdentifiers } +
+        resources.storyboards.flatMap { $0.usedImageIdentifiers }
+
+      let unusedImages = Set(allImages).subtracting(Set(allUsedImages))
+      let unusedImageGeneratedNames = unusedImages.map { SwiftIdentifier(name: $0).description }.uniqueAndSorted()
+
+      fileContents += "/* Potentially Unused Images\n"
+      fileContents += unusedImageGeneratedNames.joined(separator: "\n")
+      fileContents += "\n*/"
 
       // Write file if we have changes
       let currentFileContents = try? String(contentsOf: callInformation.outputURL, encoding: .utf8)
