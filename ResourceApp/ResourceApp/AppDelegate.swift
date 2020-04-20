cbddd80fa10d43cdd5a925438ba9b5cde9f19d4e
Tom Lokhorst <tom@lokhorst.eu>
2020-04-20 15:55:51 +0200

    Fix ResourceApp on iOS 13 with Scenes

--- ResourceApp/ResourceApp/AppDelegate.swift
+++ ResourceApp/ResourceApp/AppDelegate.swift
@@ -14,13 +14,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
-
     
 //    let defaults = UserDefaults.standard
 //    defaults.setValue(["nl"], forKey: "AppleLanguages")
 
     // Override point for customization after application launch.
     return true
+  }
+
+  @available(iOS 13.0, *)
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  @available(iOS 13.0, *)
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
   }
 
   func applicationWillResignActive(_ application: UIApplication) {
@@ -44,7 +57,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
-
 
 }
 
