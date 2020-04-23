0ef476d0ce1dfcff3274ff2112a7076411904bdb
Tom Lokhorst <tom@lokhorst.eu>
2020-04-23 17:18:12 +0200

    Split out tv app to separate example

--- Examples/RtvApp/ResourceAppTests-tvOS/ImageTests.swift
+++ Examples/RtvApp/ResourceAppTests-tvOS/ImageTests.swift
@@ -1,17 +1,16 @@
 //
 //  ImagesTests.swift
-//  ResourceAppTests
+//  ResourceAppTests-tvOS
 //
 //  Created by Mathijs Kadijk on 20-07-15.
 //  Copyright (c) 2015 Mathijs Kadijk. All rights reserved.
 //
-
 import UIKit
 import XCTest
 @testable import ResourceApp_tvOS
 
 class ImagesTests: XCTestCase {
-  
+
   func testNonNilImages() throws {
     XCTAssertNotNil(R.image.imageStackAsset())
   }
