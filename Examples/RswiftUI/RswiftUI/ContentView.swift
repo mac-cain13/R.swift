2a0e05c22a55b0c0a96a3fe9e765a909e6d7ae8a
Tom Lokhorst <tom@lokhorst.eu>
2020-09-15 13:41:38 +0200

    Add SwiftUI example

---
+++ Examples/RswiftUI/RswiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RswiftUI
+//
+//  Created by Tom Lokhorst on 2020-09-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Examples/RswiftUI/RswiftUI/RswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RswiftUIApp.swift
+//  RswiftUI
+//
+//  Created by Tom Lokhorst on 2020-09-15.
+//
+
+import SwiftUI
+
+@main
+struct RswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
