fcb1f9bf27a22b24525a8ff1319bd50db938229d
Tom Lokhorst <tom@lokhorst.eu>
2021-04-16 20:28:26 +0200

    Rename old targets with '5' stuffix

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
     .macOS(.v10_11)
   ],
   products: [
-    .executable(name: "rswift", targets: ["rswift"])
+    .executable(name: "rswift5", targets: ["rswift5"])
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
     .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.7.0")
   ],
   targets: [
-    .target(name: "rswift", dependencies: ["RswiftCore"]),
-    .target(name: "RswiftCore", dependencies: ["Commander", "XcodeEdit"]),
-    .testTarget(name: "RswiftCoreTests", dependencies: ["RswiftCore"]),
+    .target(name: "rswift5", dependencies: ["RswiftCore5"]),
+    .target(name: "RswiftCore5", dependencies: ["Commander", "XcodeEdit"]),
+    .testTarget(name: "RswiftCore5Tests", dependencies: ["RswiftCore5"]),
   ]
 )
