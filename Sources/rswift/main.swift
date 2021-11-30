ea38ef6ad1be112b8de1a650b163e6c0f755a283
Maciej Piotrowski <maciej.piotrowski@allegro.pl>
2021-11-30 14:59:23 +0100

    Fixes compilation

--- Sources/rswift/main.swift
+++ Sources/rswift/main.swift
@@ -192,6 +192,7 @@ let generate = command(
   CommanderOptions.target,
   CommanderOptions.bundleIdentifier,
   CommanderOptions.productModuleName,
+  CommanderOptions.hostingBundleName,
   CommanderOptions.infoPlistFile,
   CommanderOptions.codeSignEntitlements,
 
@@ -213,6 +214,7 @@ let generate = command(
   targetOption,
   bundleIdentifierOption,
   productModuleNameOption,
+  hostingBundleNameOption,
   infoPlistFileOption,
   codeSignEntitlementsOption,
 
@@ -241,6 +243,7 @@ let generate = command(
   let targetName = try targetOption ?? processInfo.environmentVariable(name: EnvironmentKeys.target)
   let bundleIdentifier = try bundleIdentifierOption ?? processInfo.environmentVariable(name: EnvironmentKeys.bundleIdentifier)
   let productModuleName = try productModuleNameOption ?? processInfo.environmentVariable(name: EnvironmentKeys.productModuleName)
+  let hostingBundleName = try hostingBundleNameOption ?? processInfo.environmentVariable(name: EnvironmentKeys.hostingBundleName)
   let infoPlistFile = infoPlistFileOption ?? processInfo.environment[EnvironmentKeys.infoPlistFile]
   let codeSignEntitlements = codeSignEntitlementsOption ?? processInfo.environment[EnvironmentKeys.codeSignEntitlements]
 
