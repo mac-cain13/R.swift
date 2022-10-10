3d08d7434f6c1edbfd091c657efa67a158303746
Mathijs Bernson <mathijsb@q42.nl>
2022-10-10 10:44:41 +0200

    Disable support with Swift packages

--- Plugins/RswiftPlugin/RswiftPlugin.swift
+++ Plugins/RswiftPlugin/RswiftPlugin.swift
@@ -7,58 +7,18 @@
 
 import Foundation
 import PackagePlugin
+import XcodeProjectPlugin
 
 enum RswiftPluginError: Error {
     case xcodeprojectNotFound
 }
 
 @main
-struct RswiftPlugin: BuildToolPlugin {
-    func createBuildCommands(
-        context: PackagePlugin.PluginContext,
-        target: PackagePlugin.Target
-    ) async throws -> [PackagePlugin.Command] {
-        // Directory where R.generated.swift will be stored
-        let generatedFileDirectory = context.pluginWorkDirectory
-        let generatedFilePath = generatedFileDirectory
-            .appending(subpath: "R.generated.swift")
-
-        let xcodeProjects = try FileManager.default.contentsOfDirectory(atPath: target.directory.string)
-            .filter { $0.hasSuffix(".xcodeproj") }
-        guard xcodeProjects.count == 1, let xcodeproj = xcodeProjects.first
-        else { throw RswiftPluginError.xcodeprojectNotFound }
-
-        return [
-            .buildCommand(
-                displayName: "R.swift",
-                executable: try context.tool(named: "rswift").path,
-                arguments: [
-                    "generate",
-                    "\(generatedFilePath)"
-                ],
-                environment: [
-                    "PROJECT_FILE_PATH": xcodeproj,
-                    "TARGET_NAME": target.name,
-                    "PRODUCT_MODULE_NAME": target.name,
-                    "SOURCE_ROOT": "\(target.directory)",
-
-                    // These variables must be set or R.swift will complain and not run
-                    "PRODUCT_BUNDLE_IDENTIFIER": "",
-                    "BUILT_PRODUCTS_DIR": "",
-                    "DEVELOPER_DIR": "",
-                    "SDKROOT": "",
-                    "PLATFORM_DIR": "",
-                ],
-                outputFiles: [generatedFilePath]
-            )
-        ]
+struct RswiftPlugin: BuildToolPlugin, XcodeBuildToolPlugin {
+    func createBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
+        fatalError("R.swift is not supported in Swift packages")
     }
-}
 
-#if canImport(XcodeProjectPlugin)
-import XcodeProjectPlugin
-
-extension RswiftPlugin: XcodeBuildToolPlugin {
     func createBuildCommands(
         context: XcodePluginContext,
         target: XcodeTarget
@@ -99,4 +59,3 @@ extension RswiftPlugin: XcodeBuildToolPlugin {
         ]
     }
 }
-#endif
