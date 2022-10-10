73507ebb4af93e67dca2d6f52329d92a429d04e6
Mathijs Bernson <mathijsb@q42.nl>
2022-10-10 09:53:54 +0200

    Update RswiftPlugin.swift

--- Plugins/RswiftPlugin/RswiftPlugin.swift
+++ Plugins/RswiftPlugin/RswiftPlugin.swift
@@ -8,6 +8,10 @@
 import Foundation
 import PackagePlugin
 
+enum RswiftPluginError: Error {
+    case xcodeprojectNotFound
+}
+
 @main
 struct RswiftPlugin: BuildToolPlugin {
     func createBuildCommands(
@@ -16,9 +20,14 @@ struct RswiftPlugin: BuildToolPlugin {
     ) async throws -> [PackagePlugin.Command] {
         // Directory where R.generated.swift will be stored
         let generatedFileDirectory = context.pluginWorkDirectory
-            .appending(subpath: target.name)
+            .appending(subpath: "GeneratedSources")
         let generatedFilePath = generatedFileDirectory
             .appending(subpath: "R.generated.swift")
+
+        let xcodeProjects = try FileManager.default.contentsOfDirectory(atPath: target.directory.string)
+            .filter { $0.hasSuffix(".xcodeproj") }
+        guard xcodeProjects.count == 1, let xcodeproj = xcodeProjects.first
+        else { throw RswiftPluginError.xcodeprojectNotFound }
 
         return [
             .prebuildCommand(
@@ -27,6 +36,21 @@ struct RswiftPlugin: BuildToolPlugin {
                 arguments: [
                     "generate",
                     "\(generatedFilePath)"
+                ],
+                environment: [
+                    "PROJECT_FILE_PATH": xcodeproj,
+                    "TARGET_NAME": target.name,
+                    "PRODUCT_MODULE_NAME": target.name,
+                    "SOURCE_ROOT": target.directory,
+
+                    // These variables must be set or R.swift will complain and not run
+                    "PRODUCT_BUNDLE_IDENTIFIER": "",
+                    "BUILT_PRODUCTS_DIR": "",
+                    "DEVELOPER_DIR": "",
+                    "BUILT_PRODUCTS_DIR": "",
+                    "DEVELOPER_DIR": "",
+                    "SDKROOT": "",
+                    "PLATFORM_DIR": "",
                 ],
                 outputFilesDirectory: generatedFileDirectory
             )
