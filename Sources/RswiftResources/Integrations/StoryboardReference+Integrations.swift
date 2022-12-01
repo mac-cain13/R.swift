18353e25131bc7a38b36db5cb07dcf24239630ff
Tom Lokhorst <tom@lokhorst.eu>
2022-12-01 11:22:57 +0100

    Add AppKit Storyboard integrations

--- Sources/RswiftResources/Integrations/StoryboardReference+Integrations.swift
+++ Sources/RswiftResources/Integrations/StoryboardReference+Integrations.swift
@@ -5,9 +5,6 @@
 //  Created by Tom Lokhorst on 2022-07-30.
 //
 
-import Foundation
-
-
 #if os(iOS) || os(tvOS)
 import UIKit
 
@@ -21,9 +18,8 @@ extension StoryboardReference where Self: InitialControllerContainer {
     public func instantiateInitialViewController() -> InitialController? {
         UIStoryboard(name: name, bundle: bundle).instantiateInitialViewController() as? InitialController
     }
-}
 
-extension StoryboardReference where Self: InitialControllerContainer, InitialController: UIViewController {
+
     /**
      Instantiates and returns the initial view controller in the view controller graph with native dependency injection.
 
@@ -32,11 +28,8 @@ extension StoryboardReference where Self: InitialControllerContainer, InitialCon
      - returns: The initial view controller in the storyboard.
      */
     @available(iOS 13.0, tvOS 13.0, *)
-    public func instantiateInitialViewController(
-        _ creator: @escaping (NSCoder) -> InitialController?
-    ) -> InitialController? {
-        UIStoryboard(name: name, bundle: bundle)
-            .instantiateInitialViewController(creator: creator)
+    public func instantiateInitialViewController(creator: @escaping (NSCoder) -> InitialController?) -> InitialController? where InitialController: UIViewController {
+        UIStoryboard(name: name, bundle: bundle).instantiateInitialViewController(creator: creator)
     }
 }
 
@@ -49,9 +42,8 @@ extension StoryboardViewControllerIdentifier {
     public func callAsFunction() -> ViewController? {
         UIStoryboard(name: storyboard, bundle: bundle).instantiateViewController(withIdentifier: identifier) as? ViewController
     }
-}
 
-extension StoryboardViewControllerIdentifier where ViewController: UIViewController {
+
     /**
      Instantiates and returns the view controller with the specified resource (`R.storyboard.*.*`) and native dependency injection.
 
@@ -60,11 +52,8 @@ extension StoryboardViewControllerIdentifier where ViewController: UIViewControl
      - returns: The view controller corresponding to the specified resource (`R.storyboard.*.*`).
      */
     @available(iOS 13.0, tvOS 13.0, *)
-    public func callAsFunction(
-        _ creator: @escaping (NSCoder) -> ViewController?
-    ) -> ViewController? where ViewController: UIViewController {
-        UIStoryboard(name: storyboard, bundle: bundle)
-            .instantiateViewController(identifier: identifier, creator: creator)
+    public func callAsFunction(creator: @escaping (NSCoder) -> ViewController?) -> ViewController? where ViewController: UIViewController {
+        UIStoryboard(name: storyboard, bundle: bundle).instantiateViewController(identifier: identifier, creator: creator)
     }
 }
 
@@ -90,6 +79,83 @@ extension UIStoryboard {
      */
     public func instantiateViewController<ViewController: UIViewController>(withIdentifier identifier: StoryboardViewControllerIdentifier<ViewController>) -> ViewController?  {
         self.instantiateViewController(withIdentifier: identifier.identifier) as? ViewController
+    }
+}
+#endif
+
+
+#if canImport(AppKit)
+import AppKit
+
+
+extension StoryboardReference where Self: InitialControllerContainer {
+    /**
+     Instantiates and returns the initial view controller in the view controller graph.
+
+     - returns: The initial view controller in the storyboard.
+     */
+    public func instantiateInitialViewController() -> InitialController? {
+        NSStoryboard(name: name, bundle: bundle).instantiateInitialController() as? InitialController
+    }
+
+
+    /**
+     Instantiates and returns the initial view controller in the view controller graph with native dependency injection.
+
+     - parameter creator: The function to inject dependency.
+
+     - returns: The initial view controller in the storyboard.
+     */
+    public func instantiateInitialViewController(creator: @escaping (NSCoder) -> InitialController?) -> InitialController? where InitialController: NSViewController {
+        NSStoryboard(name: name, bundle: bundle).instantiateInitialController(creator: creator)
+    }
+}
+
+extension StoryboardViewControllerIdentifier {
+    /**
+     Instantiates and returns the view controller with the specified resource (`R.storyboard.*.*`).
+
+     - returns: The view controller corresponding to the specified resource (`R.storyboard.*.*`). If no view controller is associated, this method throws an exception.
+     */
+    public func callAsFunction() -> ViewController? {
+        NSStoryboard(name: storyboard, bundle: bundle).instantiateController(withIdentifier: identifier) as? ViewController
+    }
+
+
+    /**
+     Instantiates and returns the view controller with the specified resource (`R.storyboard.*.*`) and native dependency injection.
+
+     - parameter creator: The function to inject dependency.
+
+     - returns: The view controller corresponding to the specified resource (`R.storyboard.*.*`).
+     */
+    public func callAsFunction(creator: @escaping (NSCoder) -> ViewController?) -> ViewController? where ViewController: NSViewController {
+        NSStoryboard(name: storyboard, bundle: bundle).instantiateController(identifier: identifier, creator: creator)
+    }
+}
+
+extension NSStoryboard {
+    /**
+     Creates and returns a storyboard object for the specified storyboard resource (`R.storyboard.*`) file.
+
+     - parameter resource: The storyboard resource (`R.storyboard.*`) for the specific storyboard to load
+
+     - returns: A storyboard object for the specified file. If no storyboard resource file matching name exists, an exception is thrown with description: `Could not find a storyboard named 'XXXXXX' in bundle....`
+     */
+    public convenience init<Reference: StoryboardReference>(resource: Reference) {
+        self.init(name: resource.name, bundle: resource.bundle)
+    }
+
+
+    /**
+     Instantiates and returns the view controller with the specified resource (`R.storyboard.*.*`).
+
+     - parameter resource: An resource (`R.storyboard.*.*`) that uniquely identifies the view controller in the storyboard file. If the specified resource does not exist in the storyboard file, this method raises an exception.
+
+     - returns: The view controller corresponding to the specified resource (`R.storyboard.*.*`). If no view controller is associated, this method throws an exception.
+     */
+    public func instantiateController<ViewController: NSViewController>(withIdentifier identifier: StoryboardViewControllerIdentifier<ViewController>) -> ViewController? {
+        self.instantiateController(withIdentifier: identifier.identifier) as? ViewController
     }
 }
 #endif
