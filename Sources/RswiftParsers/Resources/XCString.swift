545b75c1848cc986513eb3429b7f42b984a884b8
Fatih Doğan <fatih@fatihdogan.dev>
2023-11-07 16:37:16 +0300

    Add xcstrings support

---
+++ Sources/RswiftParsers/Resources/XCString.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+struct XCString: Decodable {
+    let sourceLanguage: String
+    let strings: [String: XCStringString]
+    let version: String
+}
+
+struct XCStringString: Decodable {
+    let localizations: [String: XCLocalization]?
+}
+
+struct XCLocalization: Decodable {
+    let stringUnit: XCStringUnit?
+    let variations: XCVariations?
+    let substitutions: [String: XCSubstitution]?
+}
+
+struct XCVariations: Decodable {
+    let plural: [String: XCPluralVariationsValue]?
+    let device: [String: XCPluralVariationsValue]?
+}
+
+struct XCPluralVariationsValue: Decodable {
+    let stringUnit: XCStringUnit?
+    let variations: XCVariations?
+}
+
+struct XCStringUnit: Decodable {
+    let value: String
+}
+
+struct XCSubstitution: Decodable {
+    let argNum: Int?
+    let formatSpecifier: String
+    let variations: XCVariations
+}
